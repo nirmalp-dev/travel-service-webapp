7b3b645c5c2154a143fbaa7c4800264320b5e3a6
Jay Gohel <jaygohel109@gmail.com>
2024-10-18 18:03:39 -0500

    deleted commented code.

--- eduvibe/eduvibe/chatbot/actions/actions.py
+++ eduvibe/eduvibe/chatbot/actions/actions.py
@@ -4,27 +4,6 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa-pro/concepts/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
 import json
 import requests
 from rasa_sdk import Action
@@ -99,17 +78,3 @@ class ActionRecommendTravelPlan(Action):
             dispatcher.utter_message(text="Sorry, there was a problem fetching travel packages.")
         
         return []
-
-# class ActionResetSlots(Action):
-#     def name(self) -> str:
-#         print("ActionResetSlots called --------------")
-#         return "reset_slots"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> list:
-#         # Resetting the slots
-#         destination = tracker.get_slot("destination")
-#         days = tracker.get_slot("days")
-#         print(f"Resetting slots: destination={destination}, days={days}")
-#         return [SlotSet("destination", None), 
-#                 SlotSet("days", None), 
-#                 SlotSet("travel_date", None)]
