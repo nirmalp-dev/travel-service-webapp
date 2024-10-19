375744f0da40ffc3a1513edfd2f3f158d6b9a994
Nirmal Patel <nirmalypatel@gmail.com>
2024-10-18 20:51:33 -0500

    removed unused components

--- eduvibe/eduvibe/src/components/course/Filters.js
+++ eduvibe/eduvibe/src/components/course/Filters.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useFilterContext } from '../../context/filter_context';
-import { getUniqueValues, formatPrice } from '../../utils';
+import {useFilterContext} from '../../context/filter_context';
+import {formatPrice, getUniqueValues} from '../../utils';
 
 const Filters = () => {
     const {
