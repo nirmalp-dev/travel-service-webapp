9e4669e112f1321c955d86ed911f023beea06c59
Nirmal Patel <nirmalypatel@gmail.com>
2024-10-18 20:05:46 -0500

    order change

--- eduvibe/eduvibe/src/common/header/Nav.js
+++ eduvibe/eduvibe/src/common/header/Nav.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
     return (
         <ul className="mainmenu">
-            <li >
+            <li>
                 <Link to="/">Home</Link>
             </li>
 
-            <li >
+            <li>
                 <Link to="/about-us">About Us</Link>
             </li>
-            
+
             <li className="">
                 <Link to="/trip-plans">Trip Plans</Link>
             </li>
