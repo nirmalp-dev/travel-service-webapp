5585aea71cee5c22f07962cbe93eacae508cd39b
Nirmal Patel <nirmalypatel@gmail.com>
2024-11-07 17:42:40 -0600

    css issue fixed

--- eduvibe/eduvibe/src/pages/chatbot/chatbot.js
+++ eduvibe/eduvibe/src/pages/chatbot/chatbot.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import imagesource from '../../assets/images/chatbot/bot.jpg';
 import '../../assets/css/chatbot/chatbot.css';
-import '../../assets/css/chatbot/style.css'
+// import '../../assets/css/chatbot/style.css'
 import { MdMoreVert } from 'react-icons/md';
 import { FaPaperPlane } from 'react-icons/fa';
 
